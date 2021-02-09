e3346fea60179bb9a8ebb8503a1eadccedfd8765
aoran <awu48@wisc.edu>
2021-02-08 21:55:39 -0600

    reordering for torch-ucc

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-UCX_HOME=/home/wisr/asinghvi/ucx XCCL_HOME=/home/wisr/aoranwu/reordering/xccl/install WITH_CUDA=/usr/local/cuda-10.2/ /home/wisr/anaconda3/envs/torch-ucc2/bin/python setup.py clean --all install 
+UCX_HOME=/home/wisr/asinghvi/ucx XCCL_HOME=/home/wisr/aoranwu/reordering/xccl/install WITH_CUDA=/usr/local/cuda-10.2/ /home/wisr/anaconda3/envs/torch-ucc2/bin/python setup.py clean --all install
